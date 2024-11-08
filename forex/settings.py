84eb6cdec8455229ba77fc43f6ae08b03497ae07
Daniel Wame <bed-com-41-20@unima.ac.mw>
2024-11-08 11:59:31 -0800

    allowed host edit

--- forex/settings.py
+++ forex/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u*9hlm2hs_q1vj6*1s$zb!fcb!m%5v&*rrsv8m9g%s7ar=_)y='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
+# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
