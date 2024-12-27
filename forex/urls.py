c633870a0c191402e243849633f2e246e3db5c89
Melvin-Kalidozo <mkalidozo3@gmail.com>
2024-12-27 10:24:12 -0800

    configured marketdata routing settings

--- forex/urls.py
+++ forex/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from authorise_deriv import views  # Corrected import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authorize/', views.authorize_user, name='authorize_user'),
+    path('marketdata/', include('marketdata.routes'))
 ]
