1245729150bec1c408839fdf5da602894b86aaf5
Daniel wame <wamedaniel9@gmail.com>
2024-12-30 00:00:03 +0200

    commenting getting forex data url

--- forex/urls.py
+++ forex/urls.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from authorise_deriv.views import authorize_user
-from authorise_deriv.views import get_forex_data
+# from authorise_deriv.views import get_forex_data
 
 # Swagger schema configuration
 schema_view = get_schema_view(
@@ -26,7 +26,7 @@ urlpatterns = [
     path("authorize/", authorize_user, name="authorize_user"),  # Authorize endpoint
       # Include URLs from the forex app
     # path("api/", include("forex.urls")),
-    path("forex_data/", get_forex_data, name="get_forex_data"),  # Forex data endpoint
+    # path("forex_data/", get_forex_data, name="get_forex_data"),  # Forex data endpoint
 
     # Swagger UI endpoints
     re_path(
