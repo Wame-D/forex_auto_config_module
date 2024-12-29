7f982252f91a4c025d5c035537742d1f5364af44
Daniel wame <wamedaniel9@gmail.com>
2024-12-30 00:08:19 +0200

    correcting error in setting.py

--- forex/settings.py
+++ forex/settings.py
@@ -37,12 +37,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'forex.urls'
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "https://forex.xhed.net/"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://forex.xhed.net/"
+]
 CORS_ALLOW_HEADERS= [
     'content-type',
-    'authorization,'
+    'authorization',
 ]
 
 TEMPLATES = [
