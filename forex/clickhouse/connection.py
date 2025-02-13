6ebf8df8e2704697dfe6a83d833a634552ed301c
Daniel wame <wamedaniel9@gmail.com>
2025-02-13 14:32:04 +0200

    adding new dashboard

--- forex/clickhouse/connection.py
+++ forex/clickhouse/connection.py
@@ -22,5 +22,3 @@ def get_clickhouse_client():
         print(f"Result:", _clickhouse_client.query("SELECT 1").result_set[0][0])
         print(f"{GREEN }Successfully connected to ClickHouse!{RESET}")
     return _clickhouse_client
-
-
